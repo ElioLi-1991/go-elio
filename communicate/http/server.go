32291f2862021d759d16ae0ab7287eef85f49e74
ElioLi <125596275@qq.com>
2022-01-07 02:26:02 +0800

    http server

--- communicate/http/server.go
+++ communicate/http/server.go
@@ -2,9 +2,11 @@ package http
 
 import (
 	"context"
+	"fmt"
 	"go-elio/communicate"
 	"net"
 	"net/http"
+	"time"
 )
 
 
@@ -14,25 +16,58 @@ var (
 
 type ServerOption func(*server)
 
-type server struct {
-	*http.Server
-	lister net.Listener
-	network string
-	addr 	string
-	err 	error
-}
 
 func Address(addr string) ServerOption {
 	return func(s *server) {
-		s.Addr = addr
+		s.address = addr
 	}
 }
 
+func Network (network string) ServerOption {
+	return func(s *server) {
+		s.network = network
+	}
+}
 
+func Lister (lister net.Listener) ServerOption {
+	return func(s *server) {
+		s.lister = lister
+	}
+}
+
+// a http server struct interface by communicate.Server
+type server struct {
+	*http.Server
+	lister net.Listener
+	timeout time.Duration
+	network string
+	address string
+	err 	error
+}
+
+// NewServer : register http server
+func NewServer (options ...ServerOption) communicate.Server {
+	s := &server{
+		network: "tcp",
+		address: ":8000",
+		timeout: 1 * time.Second,
+	}
+	for _,o := range options {
+		o(s)
+	}
+	return s
+}
+
+// start http server
 func (s *server) Start(ctx context.Context) error {
+	if s.err != nil {
+		return s.err
+	}
 	return nil
 }
 
+// stop the server
 func (s *server) Stop(ctx context.Context) error {
-	return nil
+	fmt.Println("[HTTP] server is stop")
+	return s.Shutdown(ctx)
 }
