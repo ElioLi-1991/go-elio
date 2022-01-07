6a905354cf78c5551c25490f83fd13f326b90598
lizhen <125596275@qq.com>
2022-01-07 18:49:30 +0800

    http server commit

--- communicate/http/server.go
+++ communicate/http/server.go
@@ -9,64 +9,76 @@ import (
 	"time"
 )
 
-
 var (
 	_ communicate.Server = (*server)(nil)
 )
 
 type ServerOption func(*server)
 
-
+// Address config Addr
 func Address(addr string) ServerOption {
 	return func(s *server) {
 		s.address = addr
 	}
 }
 
-func Network (network string) ServerOption {
+// Network config network
+func Network(network string) ServerOption {
 	return func(s *server) {
 		s.network = network
 	}
 }
 
-func Lister (lister net.Listener) ServerOption {
+// Lister config lister
+func Lister(lister net.Listener) ServerOption {
 	return func(s *server) {
 		s.lister = lister
+	}
+}
+
+// Timeout config timeout
+func Timeout(time time.Duration) ServerOption {
+	return func(s *server) {
+		s.timeout = time
 	}
 }
 
 // a http server struct interface by communicate.Server
 type server struct {
 	*http.Server
-	lister net.Listener
+	lister  net.Listener
 	timeout time.Duration
 	network string
 	address string
-	err 	error
+	err     error
 }
 
 // NewServer : register http server
-func NewServer (options ...ServerOption) communicate.Server {
+func NewServer(options ...ServerOption) communicate.Server {
 	s := &server{
 		network: "tcp",
 		address: ":8000",
 		timeout: 1 * time.Second,
 	}
-	for _,o := range options {
+	for _, o := range options {
 		o(s)
 	}
 	return s
 }
 
-// start http server
+// Start start http server
 func (s *server) Start(ctx context.Context) error {
 	if s.err != nil {
 		return s.err
 	}
-	return nil
+	s.BaseContext = func(net.Listener) context.Context {
+		return ctx
+	}
+	fmt.Printf("[HTTP] server is starting on addr : %s", s.lister.Addr().String())
+	return s.Serve(s.lister)
 }
 
-// stop the server
+// Stop the server
 func (s *server) Stop(ctx context.Context) error {
 	fmt.Println("[HTTP] server is stop")
 	return s.Shutdown(ctx)
